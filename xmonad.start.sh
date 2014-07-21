70b6c17d50cad0d05d1d228670c57acaae26d9d7
Sasha Rush <sasha.rush@gmail.com>
2014-07-20 21:16:36 -0400

    Configure

--- xmonad.start.sh
+++ xmonad.start.sh
@@ -4,8 +4,15 @@ trayer --edge top --align right --SetDockType true --SetPartialStrut true --expa
 
 gnome-settings-daemon & 
 
+
+
+
 gnome-power-manager &
 
 nm-applet & 
+
+eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh)
+export GNOME_KEYRING_SOCKET
+export GNOME_KEYRING_PID
 
 xmobar &
